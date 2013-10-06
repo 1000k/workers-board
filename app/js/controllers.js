a0aeb8faa547be3ddb7a834358cb3762eefd8428
SENDA <senda.keijiro@gmail.com>
2013-10-07 02:08:02 +0900

    Formats code of controller.js.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,6 @@ function getWorkers(http) {
   return http.get('workers/workers.json');
 }
 
-
 function showWorker(location, id) {
   location.path('/detail/' + id);
 }
@@ -15,67 +14,64 @@ function showTopWorker(location) {
   showWorker(location, 0);
 }
 
-function isNumber(x){ 
-    if( typeof(x) != 'number' && typeof(x) != 'string' )
-        return false;
-    else 
-        return (x == parseFloat(x) && isFinite(x));
+function isNumber(x) {
+  if (typeof(x) != 'number' && typeof(x) != 'string') return false;
+  else return (x == parseFloat(x) && isFinite(x));
 }
 
-angular.module('myApp.controllers', [])
-  .controller('WorkersListAllCtrl', ['$scope', '$http', 'WorkersCache', function($scope, $http, WorkersCache) {
+angular.module('myApp.controllers', []).controller('WorkersListAllCtrl', ['$scope', '$http', 'WorkersCache', function($scope, $http, WorkersCache) {
+  getWorkers($http).success(function(data) {
+    WorkersCache.put('workers', data);
+    $scope.workers = data;
+  });
+}])
+.controller('WorkersDetailCtrl', ['$scope', '$routeParams', '$http', '$location', 'WorkersCache', function($scope, $routeParams, $http, $location, WorkersCache) {
+  if ($routeParams.workerId) {
+    var workerId = $routeParams.workerId;
     getWorkers($http).success(function(data) {
       WorkersCache.put('workers', data);
-      $scope.workers = data;
+
+      $scope.worker = data[workerId];
     });
-  }])
-  .controller('WorkersDetailCtrl', ['$scope', '$routeParams', '$http', '$location', 'WorkersCache', function($scope, $routeParams, $http, $location, WorkersCache) {
-    if ($routeParams.workerId) {
-      var workerId = $routeParams.workerId;
-      getWorkers($http).success(function(data) {
-        WorkersCache.put('workers', data);
+  } else {
+    getWorkers($http).success(function(data) {
+      WorkersCache.put('workers', data);
 
-        $scope.worker = data[workerId];
-      });
+      var idx = Math.floor(Math.random() * data.length);
+      showWorker($location, idx);
+    });
+  }
+}])
+.controller('ControlPanel', ['$scope', '$routeParams', '$location', 'WorkersCache', function($scope, $routeParams, $location, WorkersCache) {
+
+  $scope.nextWorker = function() {
+    if (!isNumber($routeParams.workerId)) {
+      showTopWorker($location);
     } else {
-      getWorkers($http).success(function(data) {
-        WorkersCache.put('workers', data);
+      var currentId = parseInt($routeParams.workerId),
+        workers = WorkersCache.get('workers');
 
-        var idx = Math.floor(Math.random() * data.length);
-        showWorker($location, idx);
-      });
-    }
-  }])
-  .controller('ControlPanel', ['$scope', '$routeParams', '$location', 'WorkersCache', function($scope, $routeParams, $location, WorkersCache) {
-    
-    $scope.nextWorker = function() {
-      if (!isNumber($routeParams.workerId)) {
+      if ((currentId + 1) >= workers.length) {
         showTopWorker($location);
       } else {
-        var currentId = parseInt($routeParams.workerId),
-            workers = WorkersCache.get('workers');
-
-        if ((currentId + 1) >= workers.length) {
-          showTopWorker($location);
-        } else {
-          showWorker($location, currentId + 1);
-        }
+        showWorker($location, currentId + 1);
       }
     }
+  }
 
-    $scope.previousWorker = function() {
-      if (!isNumber($routeParams.workerId)) {
-        showTopWorker($location);
+  $scope.previousWorker = function() {
+    if (!isNumber($routeParams.workerId)) {
+      showTopWorker($location);
+    } else {
+      var currentId = parseInt($routeParams.workerId),
+        workers = WorkersCache.get('workers'),
+        maxId = workers.length - 1;
+
+      if ((currentId - 1) < 0) {
+        showWorker($location, maxId);
       } else {
-        var currentId = parseInt($routeParams.workerId),
-            workers = WorkersCache.get('workers'),
-            maxId = workers.length - 1;
-
-        if ((currentId - 1) < 0) {
-          showWorker($location, maxId);
-        } else {
-          showWorker($location, currentId - 1);
-        }
+        showWorker($location, currentId - 1);
       }
     }
-  }]);
+  }
+}]);
